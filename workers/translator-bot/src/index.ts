ffe1f055bca8fda1a65ca62f2399d315dd358aaf
Oleg Korobenko <okorobenko@babbel.com>
2025-06-27 14:34:52 +0200

    chore: format the code

--- workers/translator-bot/src/index.ts
+++ workers/translator-bot/src/index.ts
@@ -1,79 +1,64 @@
-import { WhatsAppAPI } from "whatsapp-api-js";
-import { Document, Image, Text } from "whatsapp-api-js/messages";
+import { WhatsAppAPI } from 'whatsapp-api-js';
+import { Document, Image, Text } from 'whatsapp-api-js/messages';
 
 // Kind reminder to not hardcode your token and secret
-const TOKEN = "YOUR_TOKEN";
-const APP_SECRET = "YOUR_SECRET";
-const VERIFY_TOKEN = "YOUR_VERIFY_TOKEN";
+const TOKEN = 'YOUR_TOKEN';
+const APP_SECRET = 'YOUR_SECRET';
+const VERIFY_TOKEN = 'YOUR_VERIFY_TOKEN';
 
 const Whatsapp = new WhatsAppAPI({
-    token: TOKEN,
-    appSecret: APP_SECRET,
-    webhookVerifyToken: VERIFY_TOKEN,
-		v: "23.0"
+	token: TOKEN,
+	appSecret: APP_SECRET,
+	webhookVerifyToken: VERIFY_TOKEN,
+	v: '23.0',
 });
 
 // Assuming get is called on a GET request to your server
 function get(request: Request) {
 	const { searchParams } = new URL(request.url);
-  Whatsapp.get({
-		"hub.mode": "subscribe",
-		"hub.verify_token": searchParams.get("hub.verify_token") ?? '',
-		"hub.challenge": searchParams.get("hub.challenge") ?? ''
+	Whatsapp.get({
+		'hub.mode': 'subscribe',
+		'hub.verify_token': searchParams.get('hub.verify_token') ?? '',
+		'hub.challenge': searchParams.get('hub.challenge') ?? '',
 	});
 }
 
 // Assuming post is called on a POST request to your server
 async function post(request: Request) {
-		const body: string = await request.text();
+	const body: string = await request.text();
 
-    await Whatsapp.post(
-        JSON.parse(body),
-        body,
-        request.headers.get("x-hub-signature-256") ?? ''
-    );
+	await Whatsapp.post(JSON.parse(body), body, request.headers.get('x-hub-signature-256') ?? '');
 }
 
 Whatsapp.on.message = async ({ phoneID, from, message, name, reply }) => {
-    console.log(
-        `User ${name} (${from}) sent to bot ${phoneID} ${JSON.stringify(
-            message
-        )}`
-    );
+	console.log(`User ${name} (${from}) sent to bot ${phoneID} ${JSON.stringify(message)}`);
 
-    let response;
+	let response;
 
-    if (message.type === "text") {
-        response = await reply(
-            new Text(`*${name}* said:\n\n${message.text.body}`),
-            true
-        );
-    }
+	if (message.type === 'text') {
+		response = await reply(new Text(`*${name}* said:\n\n${message.text.body}`), true);
+	}
 
-    if (message.type === "image") {
-        response = await reply(
-            new Image(message.image.id, true, `Nice photo, ${name}`)
-        );
-    }
+	if (message.type === 'image') {
+		response = await reply(new Image(message.image.id, true, `Nice photo, ${name}`));
+	}
 
-    if (message.type === "document") {
-        response = await reply(
-            new Document(message.document.id, true, undefined, "Our document")
-        );
-    }
+	if (message.type === 'document') {
+		response = await reply(new Document(message.document.id, true, undefined, 'Our document'));
+	}
 
-    console.log(
-        response ??
-            "There are more types of messages, such as contacts, " +
-                "locations, templates, interactive, reactions and " +
-                "all the other media types."
-    );
+	console.log(
+		response ??
+			'There are more types of messages, such as contacts, ' +
+				'locations, templates, interactive, reactions and ' +
+				'all the other media types.',
+	);
 
-    Whatsapp.markAsRead(phoneID, message.id);
+	Whatsapp.markAsRead(phoneID, message.id);
 };
 
 Whatsapp.on.sent = ({ phoneID, to, message }) => {
-    console.log(`Bot ${phoneID} sent to user ${to} ${message}`);
+	console.log(`Bot ${phoneID} sent to user ${to} ${message}`);
 };
 
 export default {
