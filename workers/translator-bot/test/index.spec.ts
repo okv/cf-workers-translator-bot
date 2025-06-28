cbcb0e2cc01de4b9907d064afccc02d72126af20
Oleg Korobenko <okorobenko@babbel.com>
2025-06-28 15:12:40 +0200

    test: add some tests

--- workers/translator-bot/test/index.spec.ts
+++ workers/translator-bot/test/index.spec.ts
@@ -6,19 +6,29 @@ import worker from '../src/index';
 // `Request` to pass to `worker.fetch()`.
 const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
 
-describe('Hello World worker', () => {
-  it('responds with Hello World! (unit style)', async () => {
+describe('translator-bot worker', () => {
+  it('responds "Not Found" when requesting the root path', async () => {
     const request = new IncomingRequest('http://example.com');
     // Create an empty context to pass to `worker.fetch()`.
     const ctx = createExecutionContext();
     const response = await worker.fetch(request, env, ctx);
     // Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
     await waitOnExecutionContext(ctx);
-    expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+    expect(await response.text()).toMatchInlineSnapshot('"Not Found"');
   });
 
-  it('responds with Hello World! (integration style)', async () => {
+  it('responds "Not Found" when requesting the root path (integration style)', async () => {
     const response = await SELF.fetch('https://example.com');
-    expect(await response.text()).toMatchInlineSnapshot(`"Hello World!"`);
+    expect(await response.text()).toMatchInlineSnapshot('"Not Found"');
+  });
+
+  it('responds with a challenge id when requesting GET /webhook', async () => {
+    const request = new IncomingRequest(
+      'http://example.com/webhook?hub.mode=subscribe&hub.challenge=1158201444&hub.verify_token=verify-token',
+    );
+    const ctx = createExecutionContext();
+    const response = await worker.fetch(request, env, ctx);
+    await waitOnExecutionContext(ctx);
+    expect(await response.text()).toMatchInlineSnapshot('"1158201444"');
   });
 });
