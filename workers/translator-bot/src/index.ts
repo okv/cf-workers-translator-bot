5f8447d0dca8450765d1826d086ddedb408d6d6c
Oleg Korobenko <okorobenko@babbel.com>
2025-06-28 14:40:50 +0200

    feat: start receiving and sending some messages

--- workers/translator-bot/src/index.ts
+++ workers/translator-bot/src/index.ts
@@ -1,34 +1,96 @@
 import { WhatsAppAPI } from 'whatsapp-api-js';
 import { Document, Image, Text } from 'whatsapp-api-js/messages';
 
-// Kind reminder to not hardcode your token and secret
-const TOKEN = 'YOUR_TOKEN';
-const APP_SECRET = 'YOUR_SECRET';
-const VERIFY_TOKEN = 'YOUR_VERIFY_TOKEN';
+async function apiFetch(url: string, token: string, options: RequestInit = {}): Promise<Response> {
+	console.log('apiFetch sending request:', { url })
+  return fetch(url, {
+    ...options,
+    headers: {
+      Authorization: `Bearer ${token}`,
+      ...(options.headers ? options.headers : undefined)
+    }
+  });
+}
 
-const Whatsapp = new WhatsAppAPI({
-	token: TOKEN,
-	appSecret: APP_SECRET,
-	webhookVerifyToken: VERIFY_TOKEN,
-	v: '23.0',
-});
+const API_VERSION = 'v23.0';
+
+interface WhatsAppMessage {
+	_type: string;
+}
+
+interface WhatsAppRequest {
+	messaging_product: string;
+	type: string;
+	to: string;
+	[key: string]: any;
+}
+
+async function sendMessage(token: string, phoneID: string, to: string, message: WhatsAppMessage, context?: string) {
+  const type = message._type;
+  const request: WhatsAppRequest = {
+    messaging_product: "whatsapp",
+    type,
+    to,
+  };
+  request[type] = message;
+  if (context) request.context = { message_id: context };
+  const promise = apiFetch(
+    `https://graph.facebook.com/${API_VERSION}/${phoneID}/messages`,
+		token,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(request)
+    }
+  );
+  const response = await (await promise).json();
+	return response;
+}
 
 // Assuming get is called on a GET request to your server
-function get(request: Request) {
+function get(api: WhatsAppAPI, request: Request): Response {
+	// const { searchParams } = new URL(request.url);
+	// Whatsapp.get({
+	// 	'hub.mode': 'subscribe',
+	// 	'hub.verify_token': searchParams.get('hub.verify_token') ?? '',
+	// 	'hub.challenge': searchParams.get('hub.challenge') ?? '',
+	// });
+
 	const { searchParams } = new URL(request.url);
-	Whatsapp.get({
+	const body = api.get({
 		'hub.mode': 'subscribe',
 		'hub.verify_token': searchParams.get('hub.verify_token') ?? '',
 		'hub.challenge': searchParams.get('hub.challenge') ?? '',
 	});
+
+	return new Response(body, {
+		status: 200,
+		headers: {'Content-Type': 'text/html'}
+	});
 }
 
 // Assuming post is called on a POST request to your server
-async function post(request: Request) {
-	const body: string = await request.text();
+async function post(api: WhatsAppAPI, request: Request): Promise<Response> {
+	const requestBody: string = await request.text();
 
-	await Whatsapp.post(JSON.parse(body), body, request.headers.get('x-hub-signature-256') ?? '');
+	const responseBody = await api.post(JSON.parse(requestBody), requestBody, request.headers.get('x-hub-signature-256') ?? '');
+	return new Response(responseBody ?? '');
 }
+
+
+export default {
+	async fetch(request: Request, env, ctx): Promise<Response> {
+
+const TOKEN = env.WHATSAPP_TOKEN;
+
+const Whatsapp = new WhatsAppAPI({
+	token: TOKEN,
+	appSecret: env.WHATSAPP_APP_SECRET,
+	webhookVerifyToken: env.WHATSAPP_VERIFY_TOKEN,
+	v: 'v23.0',
+});
 
 Whatsapp.on.message = async ({ phoneID, from, message, name, reply }) => {
 	console.log(`User ${name} (${from}) sent to bot ${phoneID} ${JSON.stringify(message)}`);
@@ -36,7 +98,11 @@ Whatsapp.on.message = async ({ phoneID, from, message, name, reply }) => {
 	let response;
 
 	if (message.type === 'text') {
-		response = await reply(new Text(`*${name}* said:\n\n${message.text.body}`), true);
+		response = await sendMessage(TOKEN, phoneID, from, new Text(`*${name}* said:\n\n${message.text.body} :wave:`));
+		// response = await reply(
+  //           new Text(`*${name}* said:\n\n${message.text.body}`),
+  //           true
+  //       );
 	}
 
 	if (message.type === 'image') {
@@ -57,22 +123,18 @@ Whatsapp.on.message = async ({ phoneID, from, message, name, reply }) => {
 	Whatsapp.markAsRead(phoneID, message.id);
 };
 
-Whatsapp.on.sent = ({ phoneID, to, message }) => {
+Whatsapp.on.sent = async ({ phoneID, to, message }) => {
 	console.log(`Bot ${phoneID} sent to user ${to} ${message}`);
 };
 
-export default {
-	async fetch(request: Request, env, ctx): Promise<Response> {
 		const url: URL = new URL(request.url);
 		const { pathname } = url;
 		if (pathname === '/webhook') {
 			switch (request.method) {
 				case 'GET':
-					get(request);
-					return new Response('GET /webhook');
+					return get(Whatsapp, request);
 				case 'POST':
-					await post(request);
-					return new Response('POST /webhook');
+					return await post(Whatsapp, request);
 				default:
 					return new Response('Not Found');
 			}
