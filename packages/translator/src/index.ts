a23a48cef01c7860b02c6dc9e3f852d327f11da1
Oleg Korobenko <oleg.korobenko@gmail.com>
2025-06-30 20:46:27 +0200

    chore: format the code

--- packages/translator/src/index.ts
+++ packages/translator/src/index.ts
@@ -5,6 +5,10 @@ import { TranslateOptions } from './index.d';
  * @param text - The input text to translate.
  * @param to - Required The language to use for translation of the input text, e.g. "en"
  */
-export async function translateText(text: string, to: string, options: TranslateOptions): Promise<string> {
+export async function translateText(
+  text: string,
+  to: string,
+  options: TranslateOptions,
+): Promise<string> {
   return text.split('').reverse().join('');
 }
